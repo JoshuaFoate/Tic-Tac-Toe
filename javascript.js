d5757386a4205df589a8ca38c94f0113a65d8342
Joshua Foate <joshuafoate@gmail.com>
2024-02-12 15:24:39 -0600

    Added the abilitiy to click the tic tac toe board and mark it with either x or o based on whose turn it is.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,131 @@
+let clickCount = 0
+
+const topLeftButton = document.querySelector('#top-left');
+topLeftButton.addEventListener('click', () => {
+    if(document.getElementById('top-left').innerText === "O" || document.getElementById('top-left').innerText === "X") {
+        return;
+    }
+    if (clickCount % 2 === 0) {
+        document.getElementById('top-left').innerText = "O";
+    }
+    else {
+        document.getElementById('top-left').innerText = "X";
+    }
+    clickCount++;
+});
+
+const topMiddleButton = document.querySelector('#top-middle');
+topMiddleButton.addEventListener('click', () => {
+    if(document.getElementById('top-middle').innerText === "O" || document.getElementById('top-middle').innerText === "X") {
+        return;
+    }
+    if (clickCount % 2 === 0) {
+        document.getElementById('top-middle').innerText = "O";
+    }
+    else {
+        document.getElementById('top-middle').innerText = "X";
+    }
+    clickCount++;
+});
+
+const topRightButton = document.querySelector('#top-right');
+topRightButton.addEventListener('click', () => {
+    if(document.getElementById('top-right').innerText === "O" || document.getElementById('top-right').innerText === "X") {
+        return;
+    }
+    if (clickCount % 2 === 0) {
+        document.getElementById('top-right').innerText = "O";
+    }
+    else {
+        document.getElementById('top-right').innerText = "X";
+    }
+    clickCount++;
+});
+
+const middleLeftButton = document.querySelector('#middle-left');
+middleLeftButton.addEventListener('click', () => {
+    if(document.getElementById('middle-left').innerText === "O" || document.getElementById('middle-left').innerText === "X") {
+        return;
+    }
+    if (clickCount % 2 === 0) {
+        document.getElementById('middle-left').innerText = "O";
+    }
+    else {
+        document.getElementById('middle-left').innerText = "X";
+    }
+    clickCount++;
+});
+
+const middleMiddleButton = document.querySelector('#middle-middle');
+middleMiddleButton.addEventListener('click', () => {
+    if(document.getElementById('middle-middle').innerText === "O" || document.getElementById('middle-middle').innerText === "X") {
+        return;
+    }
+    if (clickCount % 2 === 0) {
+        document.getElementById('middle-middle').innerText = "O";
+    }
+    else {
+        document.getElementById('middle-middle').innerText = "X";
+    }
+    clickCount++;
+});
+
+const middleRightButton = document.querySelector('#middle-right');
+middleRightButton.addEventListener('click', () => {
+    if(document.getElementById('middle-right').innerText === "O" || document.getElementById('middle-right').innerText === "X") {
+        return;
+    }
+    if (clickCount % 2 === 0) {
+        document.getElementById('middle-right').innerText = "O";
+    }
+    else {
+        document.getElementById('middle-right').innerText = "X";
+    }
+    clickCount++;
+});
+
+const bottomLeftButton = document.querySelector('#bottom-left');
+bottomLeftButton.addEventListener('click', () => {
+    if(document.getElementById('bottom-left').innerText === "O" || document.getElementById('bottom-left').innerText === "X") {
+        return;
+    }
+    if (clickCount % 2 === 0) {
+        document.getElementById('bottom-left').innerText = "O";
+    }
+    else {
+        document.getElementById('bottom-left').innerText = "X";
+    }
+    clickCount++;
+});
+
+const bottomMiddleButton = document.querySelector('#bottom-middle');
+bottomMiddleButton.addEventListener('click', () => {
+    if(document.getElementById('bottom-middle').innerText === "O" || document.getElementById('bottom-middle').innerText === "X") {
+        return;
+    }
+    if (clickCount % 2 === 0) {
+        document.getElementById('bottom-middle').innerText = "O";
+    }
+    else {
+        document.getElementById('bottom-middle').innerText = "X";
+    }
+    clickCount++;
+});
+
+const bottomRightButton = document.querySelector('#bottom-right');
+bottomRightButton.addEventListener('click', () => {
+    if(document.getElementById('bottom-right').innerText === "O" || document.getElementById('bottom-right').innerText === "X") {
+        return;
+    }
+    if (clickCount % 2 === 0) {
+        document.getElementById('bottom-right').innerText = "O";
+    }
+    else {
+        document.getElementById('bottom-right').innerText = "X";
+    }
+    clickCount++;
+});
+
 function createBoard() {
     const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     return board;
