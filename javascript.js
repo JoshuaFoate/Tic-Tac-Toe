54ad98a0a91fd12e21da80f6fa00d59313d462bf
Joshua Foate <joshuafoate@gmail.com>
2024-02-12 17:03:50 -0600

    Finished the game.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
 let clickCount = 0
 
+function createBoard() {
+    const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    return board;
+}
+let gameBoard = createBoard();
+
 const topLeftButton = document.querySelector('#top-left');
 topLeftButton.addEventListener('click', () => {
     if(document.getElementById('top-left').innerText === "O" || document.getElementById('top-left').innerText === "X") {
@@ -7,11 +13,14 @@ topLeftButton.addEventListener('click', () => {
     }
     if (clickCount % 2 === 0) {
         document.getElementById('top-left').innerText = "O";
+        gameBoard[0] = 'o';
     }
     else {
         document.getElementById('top-left').innerText = "X";
+        gameBoard[0] = 'x';
     }
     clickCount++;
+    playGame();
 });
 
 const topMiddleButton = document.querySelector('#top-middle');
@@ -21,11 +30,14 @@ topMiddleButton.addEventListener('click', () => {
     }
     if (clickCount % 2 === 0) {
         document.getElementById('top-middle').innerText = "O";
+        gameBoard[1] = 'o';
     }
     else {
         document.getElementById('top-middle').innerText = "X";
+        gameBoard[1] = 'x';
     }
     clickCount++;
+    playGame();
 });
 
 const topRightButton = document.querySelector('#top-right');
@@ -35,11 +47,14 @@ topRightButton.addEventListener('click', () => {
     }
     if (clickCount % 2 === 0) {
         document.getElementById('top-right').innerText = "O";
+        gameBoard[2] = 'o';
     }
     else {
         document.getElementById('top-right').innerText = "X";
+        gameBoard[2] = 'x';
     }
     clickCount++;
+    playGame();
 });
 
 const middleLeftButton = document.querySelector('#middle-left');
@@ -49,11 +64,14 @@ middleLeftButton.addEventListener('click', () => {
     }
     if (clickCount % 2 === 0) {
         document.getElementById('middle-left').innerText = "O";
+        gameBoard[3] = 'o';
     }
     else {
         document.getElementById('middle-left').innerText = "X";
+        gameBoard[3] = 'x';
     }
     clickCount++;
+    playGame();
 });
 
 const middleMiddleButton = document.querySelector('#middle-middle');
@@ -63,11 +81,14 @@ middleMiddleButton.addEventListener('click', () => {
     }
     if (clickCount % 2 === 0) {
         document.getElementById('middle-middle').innerText = "O";
+        gameBoard[4] = 'o';
     }
     else {
         document.getElementById('middle-middle').innerText = "X";
+        gameBoard[4] = 'x';
     }
     clickCount++;
+    playGame();
 });
 
 const middleRightButton = document.querySelector('#middle-right');
@@ -77,11 +98,14 @@ middleRightButton.addEventListener('click', () => {
     }
     if (clickCount % 2 === 0) {
         document.getElementById('middle-right').innerText = "O";
+        gameBoard[5] = 'o';
     }
     else {
         document.getElementById('middle-right').innerText = "X";
+        gameBoard[5] = 'x';
     }
     clickCount++;
+    playGame();
 });
 
 const bottomLeftButton = document.querySelector('#bottom-left');
@@ -91,11 +115,14 @@ bottomLeftButton.addEventListener('click', () => {
     }
     if (clickCount % 2 === 0) {
         document.getElementById('bottom-left').innerText = "O";
+        gameBoard[6] = 'o';
     }
     else {
         document.getElementById('bottom-left').innerText = "X";
+        gameBoard[6] = 'x';
     }
     clickCount++;
+    playGame();
 });
 
 const bottomMiddleButton = document.querySelector('#bottom-middle');
@@ -105,11 +132,14 @@ bottomMiddleButton.addEventListener('click', () => {
     }
     if (clickCount % 2 === 0) {
         document.getElementById('bottom-middle').innerText = "O";
+        gameBoard[7] = 'o';
     }
     else {
         document.getElementById('bottom-middle').innerText = "X";
+        gameBoard[7] = 'x';
     }
     clickCount++;
+    playGame();
 });
 
 const bottomRightButton = document.querySelector('#bottom-right');
@@ -119,98 +149,73 @@ bottomRightButton.addEventListener('click', () => {
     }
     if (clickCount % 2 === 0) {
         document.getElementById('bottom-right').innerText = "O";
+        gameBoard[8] = 'o';
     }
     else {
         document.getElementById('bottom-right').innerText = "X";
+        gameBoard[8] = 'x'
     }
     clickCount++;
+    playGame();
 });
 
-function createBoard() {
-    const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    return board;
-}
+const newGameButton = document.querySelector('#new-game');
+newGameButton.addEventListener('click', () => {
+    clickCount = 0
+    document.getElementById('top-left').innerText = "";
+    document.getElementById('top-middle').innerText = "";
+    document.getElementById('top-right').innerText = "";
+    document.getElementById('middle-left').innerText = "";
+    document.getElementById('middle-middle').innerText = "";
+    document.getElementById('middle-right').innerText = "";
+    document.getElementById('bottom-left').innerText = "";
+    document.getElementById('bottom-middle').innerText = "";
+    document.getElementById('bottom-right').innerText = "";
+    let gameBoard = createBoard();
+});
 
-const gameBoard = createBoard();
 function playGame() {
-    let emptySpots = 0;
-    let gameEnd = true;
-    for (i = 0; i < gameBoard.length; i++) {
-        if (gameBoard[i] === 0) {
-            emptySpots++;
-        }
+    if ((gameBoard[0] === 'x' && gameBoard[1] === 'x' && gameBoard[2] === 'x') || 
+        (gameBoard[3] === 'x' && gameBoard[4] === 'x' && gameBoard[5] === 'x') || 
+        (gameBoard[6] === 'x' && gameBoard[7] === 'x' && gameBoard[8] === 'x') || 
+        (gameBoard[0] === 'x' && gameBoard[3] === 'x' && gameBoard[6] === 'x') || 
+        (gameBoard[1] === 'x' && gameBoard[4] === 'x' && gameBoard[7] === 'x') || 
+        (gameBoard[2] === 'x' && gameBoard[5] === 'x' && gameBoard[8] === 'x') || 
+        (gameBoard[0] === 'x' && gameBoard[4] === 'x' && gameBoard[8] === 'x') || 
+        (gameBoard[2] === 'x' && gameBoard[4] === 'x' && gameBoard[6] === 'x')) {
+        console.log("Player 2 Wins!");
+        alert("Player 2 Wins!");
+        clickCount = 0
+        document.getElementById('top-left').innerText = "";
+        document.getElementById('top-middle').innerText = "";
+        document.getElementById('top-right').innerText = "";
+        document.getElementById('middle-left').innerText = "";
+        document.getElementById('middle-middle').innerText = "";
+        document.getElementById('middle-right').innerText = "";
+        document.getElementById('bottom-left').innerText = "";
+        document.getElementById('bottom-middle').innerText = "";
+        document.getElementById('bottom-right').innerText = "";
+        gameBoard = createBoard();
     }
-    for (i = 0; i < emptySpots; i++) {
-        let mark = 0;
-        if (emptySpots % 2 === 0) {
-            mark = 'o';
-        }
-        else {
-            mark = 'x';
-        }
-        let choice = 0;
-        choice = prompt("Where would you like to place your X/O");
-        if (choice.toUpperCase() === "TOP LEFT") {
-            gameBoard[0] = mark;
-        }
-        else if (choice.toUpperCase() === "TOP MIDDLE") {
-            gameBoard[1] = mark;
-        }
-        else if (choice.toUpperCase() === "TOP RIGHT") {
-            gameBoard[2] = mark;
-        }
-        else if (choice.toUpperCase() === "MIDDLE LEFT") {
-            gameBoard[3] = mark;
-        }
-        else if (choice.toUpperCase() === "MIDDLE MIDDLE") {
-            gameBoard[4] = mark;
-        }
-        else if (choice.toUpperCase() === "MIDDLE RIGHT") {
-            gameBoard[5] = mark;
-        }
-        else if (choice.toUpperCase() === "BOTTOM LEFT") {
-            gameBoard[6] = mark;
-        }
-        else if (choice.toUpperCase() === "BOTTOM MIDDLE") {
-            gameBoard[7] = mark;
-        }
-        else if (choice.toUpperCase() === "BOTTOM RIGHT") {
-            gameBoard[8] = mark;
-        }
-        else {
-            return playGame();
-        }
-        if ((gameBoard[0] === 'x' && gameBoard[1] === 'x' && gameBoard[2] === 'x') || 
-            (gameBoard[3] === 'x' && gameBoard[4] === 'x' && gameBoard[5] === 'x') || 
-            (gameBoard[6] === 'x' && gameBoard[7] === 'x' && gameBoard[8] === 'x') || 
-            (gameBoard[0] === 'x' && gameBoard[3] === 'x' && gameBoard[6] === 'x') || 
-            (gameBoard[1] === 'x' && gameBoard[4] === 'x' && gameBoard[7] === 'x') || 
-            (gameBoard[2] === 'x' && gameBoard[5] === 'x' && gameBoard[8] === 'x') || 
-            (gameBoard[0] === 'x' && gameBoard[4] === 'x' && gameBoard[8] === 'x') || 
-            (gameBoard[2] === 'x' && gameBoard[4] === 'x' && gameBoard[6] === 'x')) {
-            console.log("You win!");
-            endGame = false;
-            return endGame;
-        }
-        else if ((gameBoard[0] === 'o' && gameBoard[1] === 'o' && gameBoard[2] === 'o') || 
-            (gameBoard[3] === 'o' && gameBoard[4] === 'o' && gameBoard[5] === 'o') || 
-            (gameBoard[6] === 'o' && gameBoard[7] === 'o' && gameBoard[8] === 'o') || 
-            (gameBoard[0] === 'o' && gameBoard[3] === 'o' && gameBoard[6] === 'o') || 
-            (gameBoard[1] === 'o' && gameBoard[4] === 'o' && gameBoard[7] === 'o') || 
-            (gameBoard[2] === 'o' && gameBoard[5] === 'o' && gameBoard[8] === 'o') || 
-            (gameBoard[0] === 'o' && gameBoard[4] === 'o' && gameBoard[8] === 'o') || 
-            (gameBoard[2] === 'o' && gameBoard[4] === 'o' && gameBoard[6] === 'o')) {
-            console.log("You lose!");
-            endGame = false;
-            return endGame;
-        }
-        else {
-            playGame();
-        }
-        if (!endGame) {
-            return;
-        }
+    else if ((gameBoard[0] === 'o' && gameBoard[1] === 'o' && gameBoard[2] === 'o') ||             (gameBoard[3] === 'o' && gameBoard[4] === 'o' && gameBoard[5] === 'o') || 
+        (gameBoard[6] === 'o' && gameBoard[7] === 'o' && gameBoard[8] === 'o') || 
+        (gameBoard[0] === 'o' && gameBoard[3] === 'o' && gameBoard[6] === 'o') || 
+        (gameBoard[1] === 'o' && gameBoard[4] === 'o' && gameBoard[7] === 'o') || 
+        (gameBoard[2] === 'o' && gameBoard[5] === 'o' && gameBoard[8] === 'o') || 
+        (gameBoard[0] === 'o' && gameBoard[4] === 'o' && gameBoard[8] === 'o') || 
+        (gameBoard[2] === 'o' && gameBoard[4] === 'o' && gameBoard[6] === 'o')) {
+        console.log("Player 1 Wins!");
+        alert("Player 1 Wins!");
+        clickCount = 0
+        document.getElementById('top-left').innerText = "";
+        document.getElementById('top-middle').innerText = "";
+        document.getElementById('top-right').innerText = "";
+        document.getElementById('middle-left').innerText = "";
+        document.getElementById('middle-middle').innerText = "";
+        document.getElementById('middle-right').innerText = "";
+        document.getElementById('bottom-left').innerText = "";
+        document.getElementById('bottom-middle').innerText = "";
+        document.getElementById('bottom-right').innerText = "";
+        gameBoard = createBoard();
     }
 }
-
-playGame();
